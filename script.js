141a138c3b13c5a85ad4be5a0f564be1c0c7088c
Vens <v.kolev@alfatrex.com>
2021-12-06 15:55:44 +0100

    vens

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ let bonni = "100,00 €";
 let Dispoerhöhung = "50,00 €";
 
 kontoChuck.bank = "Sparkasse Berlin West";
-
 kontoChuck.Kontostand = parseFloat(bonni) + parseFloat(kontoChuck.Kontostand);
 kontoChuck.Dispo = parseFloat(Dispoerhöhung) + parseFloat(kontoChuck.Dispo)
 
@@ -21,7 +20,11 @@ totn_kontostand = kontoChuck.Kontostand;
 totn_dispo = kontoChuck.Dispo;
 
 kontoChuck.Kontostand = totn_kontostand.toLocaleString('de-DE', {style:'currency', currency:'EUR'});
-
 kontoChuck.Dispo = totn_dispo.toLocaleString('de-DE', {style:'currency', currency:'EUR'});
 
-console.log("Neu Bank:", kontoChuck.bank, kontoChuck);
+console.log("................");
+console.log("Neu Bank:", kontoChuck.bank);
+console.log("IBAN:", kontoChuck.iban);
+console.log("Bankleizahl:", kontoChuck.Bankleizahl);
+console.log("Kontostand:", kontoChuck.Kontostand);
+console.log("Dispo:", kontoChuck.Dispo);
