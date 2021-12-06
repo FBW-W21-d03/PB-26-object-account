951ca627024e3d523b9c46835dc7266020bc84d0
H94ssein44 <alali.52@yahoo.com>
2021-12-06 13:59:00 +0100

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+const myKonto = {
+institut : "Sparkasse Berlin Ost",
+iban    : "DE01 2345 6789 0000 1111 2222",
+blz     : "ABCDEFGH123",
+saldo   : "1000€",
+dispo   : "500€",
+password : "123456789",
+};
+
+myKonto.institut = "Sparkasse Berlin West";
+myKonto.saldo    = "1000€  Willkommensbonus 100€";
+myKonto.dispo    = "500€   Dispoerhöhung  50€";
+
+
+
+console.log(myKonto)
