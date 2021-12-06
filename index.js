2162fa0ed34180b01655221812990e2e8ecadf57
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-12-06 13:58:42 +0100

    js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Chuck = {
+    Bank:"Sparkasse Berlin Ost",
+    IBAN: "DE01 2345 6789 0000 1111 2222",
+    Bankleizahl:"ABCDEFGH123" ,
+    Baujahr: 2017,
+    Kontostand:1000,
+    DispoInHöhe:500,
+};
+Chuck.Bank ="Sparkasse Berlin West";
+Chuck.Kontostand = 1100;
+Chuck.DispoInHöhe = 550;
+
+console.log(Chuck);
