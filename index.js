085cff0de025e4fa5a3d524a97e8fd21ee63a6bf
Mazen88cdi <89069682+Mazen88cdi@users.noreply.github.com>
2021-12-06 14:22:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const bankKonto = {
     IBAN: "DE01 2345 6789 0000 1111 2222",
     Bankleizahl: "ABCDEFGH123",
     bankName:"Sparkasse Berlin Ost",
-    Kontostand: "1000€",
-    Dispo: "500€",
+    Kontostand: 1000 + "€",
+    Dispo: 500 + "€",
     
 }
 
 
 bankKonto.bankName='Sparkasse Berlin West';
-bankKonto.Kontostand = '1100';
-bankKonto.Dispo = '550';
+bankKonto.Kontostand = 1100 + '€';
+bankKonto.Dispo = 550 + '€';
 
 console.log(bankKonto);
 
