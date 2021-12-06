2ca376f8594d1d698fd917608fe0a7b1cc821505
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-06 15:23:52 +0100

    06.12.21 enza

--- javascript-object-account-enza.js
+++ javascript-object-account-enza.js
@@ -1,6 +1,30 @@
 //Aufgabenstellung - Teil 1
+let haben = 1000;
+let dispo = 500;
 
+const chuckKonto = {
+    iban: "DE0"+1+" "+2345+" "+6789+" "+"0000"+" "+1111+" "+2222,
+    blz: "ABCDEFGH123",
+    bank: "Sparkasse Berlin Ost",
+    haben:"€"+" "+haben+","+"00",
+    dispo: "€"+" "+500+","+"00",
+};
 
+console.log("Kontodaten von Chuck: " , chuckKonto);
 
+console.log("------------------")
 
 //Aufgabenstellung - Teil 2
+
+let bonus = 100;
+let dispoPlus = 50;
+
+
+chuckKonto.bank = "Sparkasse Berlin West";
+chuckKonto.haben = haben + bonus;
+chuckKonto.dispo = dispo + dispoPlus;
+
+console.log("Neue Kontodaten von Chuck: ");
+console.log("Filiale: "+chuckKonto.bank);
+console.log("Neues Guthaben: "+" "+"€"+" "+chuckKonto.haben+","+"00");
+console.log("Neuer Dispo: "+"€"+" "+chuckKonto.dispo+","+"00");
