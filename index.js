cd15f94852c8c19e42810031eac505ba8890376b
aisha2000-hub <aisha.noori2000@gmail.com>
2021-12-06 14:13:57 +0100

    Die Lösung von Aisha

---
+++ index.js
@@ -0,0 +1,22 @@
+// TEIL 1
+const chuckAccount ={
+    IBAN : "DE23456789000011112222",
+    Bankleizahl : "ABCDEFGH123",
+    bank: "sparkasse Berlin ost",
+    Kontostand: 1000,
+    Dispo : 500,
+}
+
+console.log(chuckAccount);
+
+
+// TEIL 2
+chuckAccount.Bank = "sparkass Berlin West";
+chuckAccount.Kontostand = 1000 + 100;
+chuckAccount.Dispo = 500 + 50;
+
+console.log("*****************")
+
+console.log(chuckAccount);
+
+
