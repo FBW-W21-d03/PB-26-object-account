bbb9d226db14f05910f965b065300b3401013da1
sham912411 <shamalhakim91@gmail.com>
2021-12-06 14:02:56 +0100

    lösung

--- javascript-object-account-enza.js
+++ javascript-object-account-enza.js
@@ -1,6 +1,22 @@
 //Aufgabenstellung - Teil 1
+const chuksKonto = {
+    Bank: "Sparkasse Berlin Ost",
+    IBAN: "DE01 2345 6789 0000 1111 2222",
+    Bankleizahl: "ABCDEFGH123",
+    Kontostand: "1000€",
+    Dispo: "500€"
+}
+
+chuksKonto.Bank = "Sparkasse Berlin West"
+chuksKonto.Kontostand = "1100€"
+chuksKonto.Dispo = "550€"
+
+console.log(chuksKonto)
 
 
 
 
 //Aufgabenstellung - Teil 2
+const  chuksKonto={
+gutgeschrieben: "100€",
+}
