93387661b38997d3ef4f360f59c6679bfdb4db20
Vens <v.kolev@alfatrex.com>
2021-12-06 15:10:31 +0100

    vens

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const bankKonto = {
+const kontoChuck = {
     iban: "IBAN DE01 2345 6789 0000 1111 2222",
     Bankleizahl: "ABCDEFGH123",
     bank:"Bank Sparkasse Berlin Ost",
-    Kontostand: 1000,
-    Dispo: 500
+    Kontostand: "1000,00 €",
+    Dispo: "500,00 €"
 }
 
-console.log(bankKonto.bank, bankKonto);
+console.log(kontoChuck.bank, kontoChuck);
 
-let bonni = 100;
-let Dispoerhöhung = 50
+let bonni = "100€";
+let Dispoerhöhung = 50;
 
-bankKonto.bank = "Sparkasse Berlin West";
-bankKonto.Kontostand = bonni + bankKonto.Kontostand + Dispoerhöhung;
+kontoChuck.bank = "Sparkasse Berlin West";
+
+kontoChuck.Kontostand = parseFloat(bonni) + parseFloat(kontoChuck.Kontostand);
+kontoChuck.Dispo = parseFloat(Dispoerhöhung) + parseFloat(kontoChuck.Dispo)
 
 
-console.log(bankKonto.bank, bankKonto);
+totn_kontostand = kontoChuck.Kontostand;
+totn_dispo = kontoChuck.Dispo;
+
+kontoChuck.Kontostand = totn_kontostand.toLocaleString('de-DE', {style:'currency', currency:'EUR'});
+
+kontoChuck.Dispo = totn_dispo.toLocaleString('de-DE', {style:'currency', currency:'EUR'});
+
+console.log("Neu Bank:", kontoChuck.bank, kontoChuck);
