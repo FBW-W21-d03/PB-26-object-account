146ad33597046ed7351f82011d4b5f3fe48ac68e
sprotte <metatron1972@live.de>
2021-12-06 14:08:05 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,15 @@
+const Chuck = {
+    Bankkonto: "IBAN DE01 2345 6789 0000 1111 2222",
+    Bankleizahl: "ABCDEFGH123",  
+    Sparkasse: "Berlin Ost",
+    Kontostand: 1000,
+    Dispo: 500
+    };
+    console.log(Chuck);
+    console.log("Die Iban von Chuck ist:",Chuck.Bankkonto);
+    console.log("Die Bankleitzahl von Chuck ist:",Chuck.Bankleizahl);
+    console.log("Die Sparkasse von Chuck ist:",Chuck.Sparkasse);
+    console.log("Der Konto stand von Cuck ist:",Chuck.Kontostand);
+    console.log("Das dispo von Chuck ist:",Chuck.Dispo);
+    
+    
