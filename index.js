d5236f8630b86e30e85c88e5ffd9e3eff612a9a1
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2021-12-08 10:32:23 +0100

    Account

---
+++ index.js
@@ -0,0 +1,16 @@
+const ChuckKonto = {
+    institut : "Sparkasse Berlin Ost",
+    IBAN    : "DE01 2345 6789 0000 1111 2222",
+    BLZ     : "ABCDEFGH123",
+    saldo   : "1000€",
+    dispo   : "500€",
+    password : "1234567890",
+    };
+
+
+
+ChuckKonto.saldo = "1000€ Willkommensbonus 100€";
+ChuckKonto.Dispo = "500€ Dispoerhöhung 50€";
+
+
+console.log(ChuckKonto)
